485309a3e3f5741f8ae3ae0a37b617f4ade02e58
Ege Bilecen <ege@r0m.cc>
2024-05-21 18:26:22 +0100

    implemented math functions

---
+++ rust/present80/math.rs
@@ -0,0 +1,18 @@
+fn bit_ones(size: u128) -> u128 {
+    let mut num = 0x00;
+
+    for _ in 0..size {
+        num |= 1 << size;
+    }
+
+    num
+}
+
+fn rotate_right(num: u128, bits: u128, width: u128) -> u128 {
+    let bits = if bits > width { bits % width } else { bits };
+
+    let preserved_bits = num & bit_ones(bits);
+    let bits_to_shift = num & (bit_ones(width - bits) << bits);
+
+    (bits_to_shift >> bits) | (preserved_bits << (width - bits))
+}
--- rust/present80/mod.rs
+++ rust/present80/mod.rs
@@ -1,2 +1,3 @@
 pub(crate) mod byte_vec;
 pub(crate) mod key;
+pub(crate) mod math;
