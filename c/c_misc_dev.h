42b63e7bec91dfbb6a39c6e761d33a63b80a148b
Ege Bilecen <ege@r0m.cc>
2024-05-28 11:03:07 +0100

    created header file and moved declarations to there

---
+++ c/c_misc_dev.h
@@ -0,0 +1,42 @@
+#ifndef _C_MISC_DEV_H
+#define _C_MISC_DEV_H
+
+#include "linux/miscdevice.h"
+#include "linux/fs.h"
+
+#define pr_fmt(fmt) "%s: " fmt, KBUILD_MODNAME
+
+#define DEVICE_PREFIX "present80"
+#define DEVICE_NAME_KEY DEVICE_PREFIX "_key"
+#define DEVICE_NAME_ENCRYPTION DEVICE_PREFIX "_encrypt"
+
+/* In bytes. */
+#define MAX_BUFFER_SIZE 10
+#define KEY_BUFFER_SIZE MAX_BUFFER_SIZE
+#define ENCRYPTION_BUFFER_SIZE 8
+
+#define buffer_zeroes(buff, size) memset(buff, 0, size)
+
+enum misc_dev_type { KEY_DEVICE = 0, ENCRYPTION_DEVICE };
+
+struct misc_dev_data {
+	struct mutex lock;
+
+	enum misc_dev_type type;
+	bool is_in_use;
+
+	u8 in_buffer[MAX_BUFFER_SIZE];
+	u8 out_buffer[MAX_BUFFER_SIZE];
+};
+
+struct misc_dev_group {
+	struct misc_dev_data key;
+	struct misc_dev_data encryption;
+};
+
+static int dev_open(struct inode *, struct file *);
+static ssize_t dev_read(struct file *, char __user *, size_t, loff_t *);
+static ssize_t dev_write(struct file *, const char __user *, size_t, loff_t *);
+static int dev_release(struct inode *, struct file *);
+
+#endif
