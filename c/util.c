acd3e4105f0cebe0c8a3b49385330499b435d1b8
Ege Bilecen <ege@r0m.cc>
2024-05-29 13:48:45 +0100

    updated parameter name of bytes_rotate_right

--- c/util.c
+++ c/util.c
@@ -24,27 +24,27 @@ void print_binary(const u8 *bytes, size_t size, size_t group)
 	pr_cont("\n");
 }
 
-void bytes_rotate_right(u8 *bytes, size_t size, size_t bits)
+void bytes_rotate_right(u8 *bytes, size_t size, size_t bit_count)
 {
 	size_t size_in_bits = size * 8;
 
-	if (bits > size_in_bits)
-		bits = bits % size_in_bits;
+	if (bit_count > size_in_bits)
+		bit_count = bit_count % size_in_bits;
 
-	while (bits > 0) {
-		size_t shift_count = bits % 8 == 0 ? 8 : bits % 8;
+	while (bit_count > 0) {
+		size_t shift_count = bit_count % 8 == 0 ? 8 : bit_count % 8;
 		u64 preserve_mask = shift_count == 1 ? MASK_1_BITS :
-				    bits == 2	     ? MASK_2_BITS :
-				    bits == 3	     ? MASK_3_BITS :
-				    bits == 4	     ? MASK_4_BITS :
-				    bits == 5	     ? MASK_5_BITS :
-				    bits == 6	     ? MASK_6_BITS :
-				    bits == 7	     ? MASK_7_BITS :
+				    bit_count == 2   ? MASK_2_BITS :
+				    bit_count == 3   ? MASK_3_BITS :
+				    bit_count == 4   ? MASK_4_BITS :
+				    bit_count == 5   ? MASK_5_BITS :
+				    bit_count == 6   ? MASK_6_BITS :
+				    bit_count == 7   ? MASK_7_BITS :
 						       MASK_8_BITS;
 
 		u8 fpb = bytes[0];
-		u8 pb = 0;
-		u8 npb = 0;
+		u8 pb;
+		u8 npb;
 
 		for (size_t i = 0; i < size; i++) {
 			u8 *b = bytes + i;
@@ -65,6 +65,6 @@ void bytes_rotate_right(u8 *bytes, size_t size, size_t bits)
 			      (npb >> shift_count);
 		}
 
-		bits -= shift_count;
+		bit_count -= shift_count;
 	}
 }
