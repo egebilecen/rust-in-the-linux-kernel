d546904757ea71c9f4d4a0b11e55d83691a0eec1
Ege Bilecen <ege@r0m.cc>
2024-05-30 21:31:32 +0100

    added types to benchmark script and some formatting changes

--- python/benchmark.py
+++ python/benchmark.py
@@ -2,9 +2,10 @@ import os
 import time
 from random import randbytes
 from secrets import token_bytes
+from typing import Any
 
 
-def encrypt(key: bytes, plaintext: bytes):
+def encrypt(key: bytes, plaintext: bytes) -> bytes:
     if len(key) != 10:
         raise ValueError(
             "This PRESENT-80 implementation only accepts a key of 10 bytes.")
@@ -27,7 +28,7 @@ def encrypt(key: bytes, plaintext: bytes):
     return res
 
 
-def printp(title: str = "", text: str = ""):
+def printp(title: str = "", text: str = "") -> None:
     if title == "" and text == "":
         print()
         return
@@ -40,15 +41,16 @@ def printp(title: str = "", text: str = ""):
     print()
 
 
-def get_time():
+def get_time() -> float:
     return time.time_ns()
 
 
-def get_time_result(time_ns):
+def get_time_result(time_ns: float) -> dict[str, Any]:
     return {
         "ns": time_ns,
         "us": time_ns / float(10 ** 3),
-        "ms": time_ns / float(10 ** 6)
+        "ms": time_ns / float(10 ** 6),
+        "s": time_ns / float(10 ** 9)
     }
 
 
@@ -66,8 +68,8 @@ for _ in range(TOTAL_PLAINTEXTS):
     plaintext_list.append(randbytes(8))
 
 start_time = get_time()
-sum_encrypt_time = 0
-end_time = 0
+sum_encrypt_time = 0.0
+end_time = 0.0
 
 for key in key_list:
     for plaintext in plaintext_list:
@@ -82,11 +84,11 @@ time_diff_ns = end_time - start_time
 time_result = get_time_result(time_diff_ns)
 time_result["avg"] = get_time_result(sum_encrypt_time / TOTAL_ENCRYPTION)
 
-printp("TOTAL KEYS", TOTAL_KEYS)
-printp("TOTAL PLAINTEXTS", TOTAL_PLAINTEXTS)
-printp("TOTAL ENCRYPTION", TOTAL_ENCRYPTION)
+printp("TOTAL KEYS", str(TOTAL_KEYS))
+printp("TOTAL PLAINTEXTS", str(TOTAL_PLAINTEXTS))
+printp("TOTAL ENCRYPTION", str(TOTAL_ENCRYPTION))
 printp("TOTAL TIME",
-       "{:.2f}ns / {:.2f}us".format(time_result["ns"], time_result["us"]))
+       "{:.2f}sec / {:.2f}ms".format(time_result["s"], time_result["ms"]))
 printp("AVG. ENCRYPTION TIME",
        "{:.2f}ns / {:.2f}us".format(time_result["avg"]["ns"],
                                     time_result["avg"]["us"]))
